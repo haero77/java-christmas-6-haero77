2d98559efad2c51603b227aedd5370d8a3525a55
haero77 <shineecard5@gmail.com>
2023-11-13 01:01:39 +0900

    feat: 특별 할인 기능

---
+++ src/main/java/christmas/domain/event/discount/SpecialDiscountPolicy.java
@@ -0,0 +1,43 @@
+package christmas.domain.event.discount;
+
+import christmas.domain.event.Reservation;
+import java.time.LocalDate;
+import java.util.List;
+
+public class SpecialDiscountPolicy implements DiscountPolicy {
+
+    /*- [ ] 기간: 이벤트 달력에 별이 있는 날(3, 10, 17, 24, 31, 25일)
+  - [ ] 방식: 총주문 금액에서 1,000원 할인*/
+
+    private static final List<LocalDate> DISCOUNT_DATES = List.of(
+            LocalDate.of(2023, 12, 3),
+            LocalDate.of(2023, 12, 10),
+            LocalDate.of(2023, 12, 17),
+            LocalDate.of(2023, 12, 24),
+            LocalDate.of(2023, 12, 25),
+            LocalDate.of(2023, 12, 31)
+    );
+
+    private static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
+
+    private final Reservation reservation;
+
+    public SpecialDiscountPolicy(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    @Override
+    public DiscountAmount discount() {
+        if (meetsDateRangeCriterion()) {
+            return DiscountAmount.from(DEFAULT_DISCOUNT_AMOUNT);
+        }
+        return DiscountAmount.from(0);
+    }
+
+    private boolean meetsDateRangeCriterion() {
+        return DISCOUNT_DATES.stream()
+                .map(LocalDate::getDayOfMonth)
+                .anyMatch(date -> date == this.reservation.getVisitDate());
+    }
+
+}
