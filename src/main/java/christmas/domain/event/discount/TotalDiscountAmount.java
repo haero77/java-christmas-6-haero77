adaf9f40b8e324bb951246f5f3c4537f4bc4d231
haero77 <shineecard5@gmail.com>
2023-11-13 04:08:46 +0900

    feat: 총 할인 금액 계산 객체 구현

---
+++ src/main/java/christmas/domain/event/discount/TotalDiscountAmount.java
@@ -0,0 +1,11 @@
+package christmas.domain.event.discount;
+
+import christmas.domain.common.Amount;
+
+public class TotalDiscountAmount extends Amount {
+
+    protected TotalDiscountAmount(long amount) {
+        super(amount);
+    }
+
+}
