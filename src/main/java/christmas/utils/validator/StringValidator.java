25ff84c4d49ce93c1ba239b49266974003c85c29
haero77 <shineecard5@gmail.com>
2023-11-12 19:52:03 +0900

    feat: 문자열 검증, 변환 유틸 클래스 구현

--- src/main/java/christmas/utils/validator/StringValidator.java
+++ src/main/java/christmas/utils/validator/StringValidator.java
@@ -1,17 +1,37 @@
 package christmas.utils.validator;
 
-import christmas.utils.formatter.ErrorMessageFormatter;
+import java.util.Objects;
 
 public class StringValidator {
 
     public static void validateInteger(String literal) {
-        validateInteger(literal, ErrorMessageFormatter.format("literal %s is not an integer.".formatted(literal)));
+        validateInteger(literal, "literal %s is not an integer.".formatted(literal));
     }
 
     public static void validateInteger(String literal, String message) {
         if (!isInteger(literal)) {
             throw new IllegalArgumentException(message);
         }
+    }
+
+    public static void validateNotEmpty(String literal) {
+        validateNotEmpty(literal, "literal %s is empty".formatted(literal));
+    }
+
+    public static void validateNotEmpty(String literal, String message) {
+        if (!hasContents(literal)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private static boolean hasContents(String literal) {
+        if (Objects.isNull(literal)) {
+            return false;
+        }
+        if (literal.isEmpty()) {
+            return false;
+        }
+        return !literal.isBlank();
     }
 
     private static boolean isInteger(String literal) {
